6a0944b7e0c28493391cfa311ac5c78846bac0be
Viacheslav Alekseev <VA_Alekseev@outlook.com>
2022-03-23 20:09:19 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,58 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskDialog = Autodesk.Revit.UI.TaskDialog;
+
+namespace ExportPIC
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+     
+            string path = "";
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                path = dialog.FileName+"\\ExportPIC.jpg";
+            }
+            else
+            {
+                return Result.Succeeded;
+            }
+
+            ImageExportOptions imgOptions = new ImageExportOptions
+            {
+                ZoomType = ZoomFitType.FitToPage,
+                PixelSize = 2024,
+                FilePath = path,
+                FitDirection = FitDirectionType.Horizontal,
+                HLRandWFViewsFileType = ImageFileType.JPEGLossless,
+                ImageResolution = ImageResolution.DPI_600,
+            };
+
+            using (var ts = new Transaction(doc, "export"))
+            {
+                ts.Start();
+                doc.ExportImage(imgOptions);
+                ts.RollBack();
+            }
+            TaskDialog.Show("Cообщение", "Экспорт в формат изображения завершено");
+            return Result.Succeeded;
+        }
+    }
+}
